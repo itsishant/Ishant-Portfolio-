d3da5f42cbc6d7036bf6bba06d0b17b6337ac89b
itsishant <modakmania@gmail.com>
2025-12-05 12:27:56 +0530

    remove background beams for smooth ui

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import { BackgroundBeams } from "./Components/ui/background-beams";
 export const App = () => {
   return (
     <div className="relative min-h-screen w-full bg-black text-white overflow-hidden">
-      <BackgroundBeams />
       <div className="relative z-10">
         <Router>
           <Header />
