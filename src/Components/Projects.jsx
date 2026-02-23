54295cd7672f368bcbd81f6ac3829794d73f173b
itsishant <modakmania@gmail.com>
2026-02-23 22:46:08 +0530

    udpated with projects name

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -62,7 +62,7 @@ export const Projects = () => {
                 : "text-gray-500 hover:text-gray-700"
           }`}
         >
-          Blockchain
+          Android
           {activeTab === "blockchain" && (
             <motion.div
               layoutId="projectTab"
